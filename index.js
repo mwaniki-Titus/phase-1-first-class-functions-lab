c75f47181b43cdfd9f6e3a756a0962fbf95eb3e5
Titus Munyoki <titus.munyoki@student.moringaschool.com>
2023-06-15 23:21:05 +0300

    completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0,2);
+}
+function returnLastTwoDrivers(drivers){
+    return drivers.slice(-2)
+    
+}
+const selectingDrivers=[returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier){
+    return function(fare){
+        return fare * multiplier;
+    }
+
+}
+function fareDoubler(fare){
+    return fare * 2;
+}
+function fareTripler(fare){
+    return fare * 3;
+}
+function selectDifferentDrivers(drivers, selectorFunction) {
+    return selectorFunction(drivers);
+    
+}
